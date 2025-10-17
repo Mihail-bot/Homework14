d9c64927c165ee15d23dae4cb66b8f14ce18ddcf
Miha <shmihail85@yandex.ru>
2025-10-17 15:36:34 +0300

    Hw17

--- src/org/skypro/skyshop/article/SearchEngine.java
+++ src/org/skypro/skyshop/article/SearchEngine.java
@@ -5,14 +5,6 @@ import java.util.List;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
-//class CustomComparator implements Comparator<String> {
-  //  @Override
- //   public int compare(String o1, String o2) {
-        // Логика сравнения строк, зависит от вашей задачи
-//        return o1.compareTo(o2); // Простое сравнение строк лексикографически
-//    }
-//}
-
 public class SearchEngine {
     private Set<Searchable> data = new HashSet<>();
 
